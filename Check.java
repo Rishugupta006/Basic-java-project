9258e3cdd111d7d91cae77317e9f2ed5d3742436
Rishu kumar gupta <rishugupta8084@gmail.com>
2026-06-04 21:54:19 -0700

    Add files via upload

---
+++ Check.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Check{
+    public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no.:");
+float  a=sc.nextFloat();
+if(a>=0)
+{
+    System.out.println("positive");}
+else{
+
+    System.out.println("negative"); 
+}
+}}
