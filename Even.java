1e19664b43e6e68d6a4daa00a08d507b610a0f42
Rishu kumar gupta <rishugupta8084@gmail.com>
2026-06-08 20:34:13 -0700

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Even{
+    public static void main(String arg[])
+    {
+        for(int a=9;a<100;a++)
+        {
+            if(a%2==0){
+
+            
+            System.out.println(a);
+        }}
+    }
+}
