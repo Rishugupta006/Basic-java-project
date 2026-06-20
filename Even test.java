9cd2d12ac09cc67543258f56949f2c4a6b764467
Rishu kumar gupta <rishugupta8084@gmail.com>
2026-06-19 22:56:57 -0700

    Add files via upload

---
+++ Even test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Even Test{
+    public static boolean iseven(int a){
+        return a%2==0
+    }
+    public  static void main(Stringarg[]){
+        int p=5;
+        int q=4;
+        System.out.println(p+"is even"+iseven(p))
+         System.out.println(q+"is even"+iseven(q))
+    }
+}
