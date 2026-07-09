cfbce8bf09d2651cd0b44d1bdac7a3d2d3b4c310
Rishu kumar gupta <rishugupta8084@gmail.com>
2026-07-09 09:49:33 -0700

    Add files via upload

---
+++ Stringletter.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class String{
+    public static void printletter(String str){
+        for(int i=0;i<Str.length();i++){
+            system.out.println(str.charAt(i)+"");
+        }
+    }
+    String firstname="shradha";
+    String lastname="khapra";
+    String fullname=firstname+""+lastname;
+    // public static void main(String arg[]){
+    printletter(str);
+}
